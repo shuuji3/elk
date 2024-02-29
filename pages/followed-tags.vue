53d642a6ff68fc5f92064b44909088ade482e698
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-03-01 00:39:51 +0900

    feat: add ordering button alphabetically or chronologically

--- pages/followed-tags.vue
+++ pages/followed-tags.vue
@@ -13,6 +13,15 @@ const paginator = client.value.v1.followedTags.list({
 useHydratedHead({
   title: () => t('nav.followed_tags'),
 })
+
+const ordering = ref<'alphabetical' | 'chronological'>('alphabetical')
+
+function toggleOrdering() {
+  if (ordering.value === 'alphabetical')
+    ordering.value = 'chronological'
+  else
+    ordering.value = 'alphabetical'
+}
 </script>
 
 <template>
@@ -22,6 +31,20 @@ useHydratedHead({
         <div class="i-ri:hashtag" />
         <span>{{ t('nav.followed_tags') }}</span>
       </NuxtLink>
+    </template>
+
+    <template #actions>
+      <button
+        flex rounded-4 p2
+        hover:bg-active cursor-pointer transition-100
+        :title="t('settings.notifications.show_btn')"
+      >
+        <span
+          aria-hidden="true"
+          :class="ordering === 'alphabetical' ? 'i-ri:sort-alphabet-asc' : 'i-ri:sort-number-asc'"
+          @click="toggleOrdering"
+        />
+      </button>
     </template>
 
     <TagCardPaginator v-bind="{ paginator }" />
