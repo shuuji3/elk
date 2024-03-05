09838742a548477dc32df14fdced36921ce2dcb2
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-03-05 23:34:13 +0900

    chore: migrate `.eslintrc` to `eslint.config.js`

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  ignorePatterns: ['!pages/public'],
+  overrides: [
+    {
+      files: ['locales/**.json'],
+      rules: {
+        'jsonc/sort-keys': 'error',
+      },
+    },
+  ],
+  rules: {
+    'vue/no-restricted-syntax': ['error', {
+      selector: 'VElement[name=\'a\']',
+      message: 'Use NuxtLink instead.',
+    }],
+    'n/prefer-global/process': 'off',
+  },
+})
