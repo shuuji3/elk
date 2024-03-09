b1291be4f09b6d12e27e78a6b622055a2fe375a5
TAKAHASHI Shuuji <shuuji3@gmail.com>
2024-03-09 17:33:21 +0900

    feat: show Hashtags page intro notice

--- pages/hashtags.vue
+++ pages/hashtags.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import TagCardPaginator from '~/components/tag/TagCardPaginator.vue'
+
+import { STORAGE_KEY_HIDE_EXPLORE_TAGS_TIPS } from '~/constants'
+
 definePageMeta({
   middleware: 'auth',
 })
@@ -10,9 +14,13 @@ const paginator = client.value.v1.followedTags.list({
   limit: 20,
 })
 
+const hideExploreTagsTips = useLocalStorage(STORAGE_KEY_HIDE_EXPLORE_TAGS_TIPS, false)
+
 useHydratedHead({
   title: () => t('nav.hashtags'),
 })
+
+const { items } = usePaginator(paginator, toRef(() => undefined), undefined)
 </script>
 
 <template>
@@ -23,6 +31,17 @@ useHydratedHead({
         <span>{{ t('nav.hashtags') }}</span>
       </NuxtLink>
     </template>
+
+    <CommonAlert v-if="items.length === 0 && !hideExploreTagsTips" @close="hideExploreTagsTips = true">
+      <i18n-t keypath="tooltip.hashtags_intro" tag="p">
+        <NuxtLink
+          :to="`/${currentServer}/explore/tags`"
+          text-primary hover:underline focus:underline focus:outline-none
+        >
+          {{ t('tooltip.hashtags_intro_link') }}
+        </NuxtLink>
+      </i18n-t>
+    </CommonAlert>
 
     <TagCardPaginator v-bind="{ paginator }" />
   </MainContent>
